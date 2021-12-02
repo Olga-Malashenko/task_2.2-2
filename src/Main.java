2be5789ebe042aa32285ccb460a6e36b4b44367c
Olga Malashenko <olga_birjukova@mail.ru>
2021-12-02 20:31:20 +0300

    new data

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double bmi = service.calculate(62,176);
+        double bmi = service.calculate(62,1.76);
         System.out.println(bmi);
     }
 }
